dd4da12908e5be9c3246512042c08b48f85d114b
Okamoto <85035999+Okamoto148@users.noreply.github.com>
2023-01-09 20:27:55 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,170 @@
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie'
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignIn() {
+
+  useEffect(()=>{
+    const novoToken=localStorage.getItem('token');
+    const novoRemember=localStorage.getItem('remember');
+
+    
+    if(novoRemember){
+      setRemember(novoRemember);
+      if(novoToken){
+        setToken(novoToken);
+        
+        axios
+        .post("./login",{
+           token: novoToken,
+          remember: novoRemember
+        });
+      };
+    };
+
+    //if(novoRemember && (novoToken&&novoToken!=='Login inválido!')){
+     // document.location.pathname = '/profile';
+  //  }
+    
+  },[])
+
+  const [token, setToken]=useState('');
+  const [remember,setRemember]=useState(false);
+  console.log(remember)
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+      axios
+        .post("./login",{
+           user: data.get('email'),
+           pass: data.get('password')
+})
+     .then((response) => {
+       if(response.data==='Login inválido!'){
+         alert(response.data)
+       }else{
+         setToken(response.data);
+         localStorage.setItem('token',response.data);
+    document.location.pathname = '/profile';
+       }
+       }
+          )
+     .catch((err) => {
+       console.error("ops! ocorreu um erro" + err);
+     })
+  };
+
+
+  console.log(token)
+
+      function togleRemember(event){
+        const novoRemember = remember?false:true;
+        setRemember(novoRemember);
+        localStorage.setItem('remember',novoRemember);
+      }
+      
+    
+  
+
+  
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value={remember} color="primary" onChange={togleRemember} checked={remember}/>}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const next = require('next');
+
+const app = express();
+const jwt = require('jsonwebtoken');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
+
+const blacklist = [];
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+nextApp.prepare().then(() => {
+
+  app.get('/profile', verifyJWT, nextHandler);
+
+
+function verifyJWT(req, res, next) {
+  var token = app.get('token');
+  if (!token || token==='Login inválido!') return res.redirect('/');
+
+  const index = blacklist.findIndex(item => item === token);
+  if(index !== -1) return res.status(401).end();
+
+  jwt.verify(token, process.env.SECRET, function (err, decoded) {
+    if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
+
+    // se tudo estiver ok, salva no request para uso posterior
+    req.userId = decoded.id;
+
+    app.set('token','');
+    next();
+  });
+}
+
+app.post('/login', (req, res, next) => {
+  console.log(req.body.token)
+  //esse teste abaixo deve ser feito no seu banco de dados
+  if (req.body.user === 'leandro@leandro' && req.body.pass === 'leandro') {
+    //auth ok
+    const userId = 1; //esse id viria do banco de dados
+
+    if(req.body.token){
+      var token = req.body.token;
+    }else{
+      var token = jwt.sign({ userId }, process.env.SECRET, {
+      expiresIn: 1000 // expires in 5min
+    });
+    }
+    app.set('token', token);
+    return res.send(token);
+
+    
+  }
+
+  res.send('Login inválido!');
+})
+
+
+
+
+  // Message Sending
+  app.get("/custom-route", (req, res) => {
+    res.send("Custom Route!!")
+  })
+
+  app.get("*", nextHandler)
+  app.post('*', nextHandler)
+
+  app.listen(port, err => {
+    if (err) throw err
+    console.log(`Listening on port ${port}`)
+  })
+})
